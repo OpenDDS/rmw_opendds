d2b2e3fe27f52cc849f1e6cefd6e995b42dacdbe
Brandon Sanders <brandon@alicorn.io>
2018-08-02 14:49:36 -0400

    Begin adding Python type support

--- rosidl_typesupport_opendds_c/rosidl_typesupport_opendds_c-extras.cmake
+++ rosidl_typesupport_opendds_c/rosidl_typesupport_opendds_c-extras.cmake
@@ -2,3 +2,8 @@ find_package(ament_cmake REQUIRED)
 find_package(opendds_cmake_module REQUIRED)
 find_package(OpenDDS MODULE)
 list(INSERT CMAKE_MODULE_PATH 0 "${rosidl_typesupport_opendds_c_DIR}/Modules")
+
+ament_register_extension(
+    "rosidl_generate_interfaces"
+    "rosidl_typesupport_opendds_c"
+    "rosidl_typesupport_opendds_c_generate_interfaces.cmake")
