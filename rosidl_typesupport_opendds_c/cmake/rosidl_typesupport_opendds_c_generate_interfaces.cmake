07a3b3921ed3ba390da793ce477a094c67e7e052
Brandon Sanders <brandon@alicorn.io>
2018-08-08 13:28:10 -0400

    Update C interface generator to properly invoke the ROS-2 OMG IDL generator. Next step is to invoke the TAO and OpenDDS IDL parsers.

--- rosidl_typesupport_opendds_c/cmake/rosidl_typesupport_opendds_c_generate_interfaces.cmake
+++ rosidl_typesupport_opendds_c/cmake/rosidl_typesupport_opendds_c_generate_interfaces.cmake
@@ -1,14 +1,63 @@
+# Name of this generator.
+set(_rosidl_typesupport_name "rosidl_typesupport_opendds_c")
 
 # Default _target_suffix is the target that invoked this one;
 # e.g., __rosidl_generator_c. We must change this to our package
 # name to be compliant with the expectations of generators.
-set(_target_suffix "__rosidl_typesupport_opendds_c")
+set(_target_suffix "__${_rosidl_typesupport_name}")
 
-message("Generating OpenDDS C Interfaces... [${PROJECT_NAME}]")
-message("For: Target ${rosidl_generate_interfaces_TARGET} with suffix ${_target_suffix}")
+# Path where generated interfaces and intermediaries will be stored.
+set(_output_path "${CMAKE_CURRENT_BINARY_DIR}/${_rosidl_typesupport_name}/${PROJECT_NAME}")
 
-foreach(_idl_file ${rosidl_generate_interfaces_IDL_FILES})
-    message("    Generating IDL file: ${_idl_file}")
-endforeach()
+# Print candidate MSG and SRV files to process into IDLs.
+message("Generating OpenDDS C interfaces for ${rosidl_generate_interfaces_TARGET} in ${_output_path}.")
 
+# Generate OMG IDLs from MSG and SRV files.
+message("    Converting ROS-2 MSG/SRV files to OMG IDLs.")
+rosidl_generate_dds_interfaces(
+  "${rosidl_generate_interfaces_TARGET}${_target_suffix}__idls"
+  IDL_FILES ${rosidl_generate_interfaces_IDL_FILES}
+  DEPENDENCY_PACKAGE_NAMES ${target_dependencies}
+)
+
+# Print output location of OMG IDLs.
+get_filename_component(_msg_destination "${_msg_destination}" ABSOLUTE)
+get_filename_component(_srv_destination "${_srv_destination}" ABSOLUTE)
+message("    Installed OMG IDLs for msg files [${_generated_msg_files}] to ${_msg_destination}")
+message("    Installed OMG IDLs for srv files [${_generated_srv_files}] to ${_srv_destination}")
+
+# Install OMG IDL target.
+add_library(${rosidl_generate_interfaces_TARGET}${_target_suffix} SHARED
+  ${_generated_msg_files}
+  ${_generated_srv_files})
+
+# Set OMG IDL target language.
+set_target_properties(${rosidl_generate_interfaces_TARGET}${_target_suffix} PROPERTIES LINKER_LANGUAGE CXX)
+
+# Add OMG IDL target dependencies.
+add_dependencies(
+  ${rosidl_generate_interfaces_TARGET}
+  ${rosidl_generate_interfaces_TARGET}${_target_suffix}
+)
+add_dependencies(
+  ${rosidl_generate_interfaces_TARGET}${_target_suffix}
+  ${rosidl_generate_interfaces_TARGET}__cpp
+)
+
+# Install OMG IDL target if enabled.
+if(NOT rosidl_generate_interfaces_SKIP_INSTALL)
+  install(
+    TARGETS ${rosidl_generate_interfaces_TARGET}${_target_suffix}
+    ARCHIVE DESTINATION lib
+    LIBRARY DESTINATION lib
+    RUNTIME DESTINATION bin
+  )
+
+  rosidl_export_typesupport_libraries(
+    ${_target_suffix}
+    ${rosidl_generate_interfaces_TARGET}${_target_suffix}
+  )
+endif()
+
+# Complete!
 message("Generated OpenDDS C Interfaces!")
