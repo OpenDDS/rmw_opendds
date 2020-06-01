615d3825a20ec021300d2eff2dd90b753c974473
Jiang Li <lij@objectcomputing.com>
2020-06-01 15:05:06 -0500

    added TODO: ... when service typesupport is ready.

--- rmw_opendds_cpp/src/rmw_service.cpp
+++ rmw_opendds_cpp/src/rmw_service.cpp
@@ -179,7 +179,10 @@ rmw_create_service(
     info->replier_ = info->callbacks_->create_replier(node_info->participant,
       request_topic.c_str(), response_topic.c_str(), publisher, subscriber, &rmw_allocate);
     if (!info->replier_) {
+      return service;
+/* TODO: delete the line above and uncomment the line below when service typesupport is ready.
       throw std::string("failed to create_replier");
+*/
     }
 
     info->request_reader_ = static_cast<DDS::DataReader*>(
